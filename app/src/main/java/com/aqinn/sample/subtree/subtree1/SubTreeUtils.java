9073ea33bd71cfacb726047d528c260d4f778767
allanzhong <allanzhong@tencent.com>
2023-03-28 18:04:13 +0800

    subtree edit

--- app/src/main/java/com/aqinn/sample/subtree/subtree1/SubTreeUtils.java
+++ app/src/main/java/com/aqinn/sample/subtree/subtree1/SubTreeUtils.java
@@ -6,6 +6,7 @@ import android.widget.Toast;
 public class SubTreeUtils {
 
     public static void sayHi(Context context) {
+        // 尝试在 subtree 仓库修改代码 1
         Toast.makeText(context, "Hi, here is sub tree 1.", Toast.LENGTH_SHORT).show();
     }
 
