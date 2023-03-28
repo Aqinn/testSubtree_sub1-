f84851410208b0d345107f98afdd33a4ae93d07f
allanzhong <allanzhong@tencent.com>
2023-03-28 18:03:09 +0800

    在主仓库测试添加subtree的代码

--- app/src/main/java/com/aqinn/sample/subtree/subtree1/SubTreeUtils.java
+++ app/src/main/java/com/aqinn/sample/subtree/subtree1/SubTreeUtils.java
@@ -6,6 +6,7 @@ import android.widget.Toast;
 public class SubTreeUtils {
 
     public static void sayHi(Context context) {
+        // test: root repo add.
         Toast.makeText(context, "Hi, here is sub tree 1.", Toast.LENGTH_SHORT).show();
     }
 
