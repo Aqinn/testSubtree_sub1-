fbed8d09fd64021bd8ebc5a2d0a540bf4ae3bf51
allanzhong <allanzhong@tencent.com>
2023-03-28 17:40:58 +0800

    init

---
+++ app/src/main/java/com/aqinn/sample/subtree/subtree1/SubTreeUtils.java
@@ -0,0 +1,12 @@
+package com.aqinn.sample.subtree.subtree1;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class SubTreeUtils {
+
+    public static void sayHi(Context context) {
+        Toast.makeText(context, "Hi, here is sub tree 1.", Toast.LENGTH_SHORT).show();
+    }
+
+}
